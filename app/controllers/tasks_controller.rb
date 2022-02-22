111bb83021aff17ab55ffb0411df755cb8b8870a
Andrey Kryuchkov <askryuchkov@ya.ru>
2022-02-22 16:22:10 +0800

    task manager with no CSS

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      redirect_to :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      redirect_to edit_task_path(@task)
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list
+  # get 'tasks', to: 'tasks#index'
+  # # creating new task
+  # get '/tasks/new', to: "tasks#new"
+  # post '/tasks', to: "tasks#add"
+
+  # #show the task page
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # #edit task
+  # get '/tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # delete
+  # delete 'tasks/:id', to: "tasks#destroy", as: :destroy
+  resources :tasks
 end
